a150bfff1d76352aa334fa7cef1bf30f39f7819a
Paper Girl <jamie.rose.528@gmail.com>
2021-11-18 14:45:03 -0800

    made time move live

--- assets/java.js
+++ assets/java.js
@@ -1,3 +1,6 @@
 var now = moment().format("MMM Do, YYYY, HH:mm:ss")
 $("#current-time-day").text(now);
-
+setInterval(() => {
+    now = moment().format("MMM Do, YYYY, HH:mm:ss")
+$("#current-time-day").text(now);
+}, 1000);
